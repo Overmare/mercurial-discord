87c3753d89ef0db6b7297c102920ef75d7330889
Ansis Māliņš <ansis.malins@gmail.com>
2021-08-07 00:21:39 +0200

    Fixed bytes vs strings issues

--- hooks.py
+++ hooks.py
@@ -4,35 +4,35 @@ EXTENSIONS = [ "bat", "cginc", "compute", "cpp", "cs", "groovy", "h", "hgignore"
 import json, os, re, urllib.error, urllib.request
 
 def EscapeMarkdown(str):
-    return str.re
+    return str # TODO
 
 def incoming(ui, repo, node, **kwargs):
     ctx = repo[node]
 
     pattern = r"\.(?:" + "|".join(EXTENSIONS) + ")$"
-    if not any(re.search(pattern, i) != None for i in ctx.files()):
+    if not any(re.search(pattern, i.decode("utf-8")) != None for i in ctx.files()):
         return
 
     try:
         secretsPath = os.path.dirname(os.path.abspath(__file__)) + "/secrets.txt";
         secrets = json.load(open(secretsPath))
     except (IOError, ValueError) as ex:
-        ui.write("Discord incoming hook could not load secrets because " + str(ex))
+        ui.write(("Discord incoming hook could not load secrets because " + str(ex) + "\n").encode("utf-8"))
         return
 
-    shortId = ctx.hex()[:12]
+    shortId = ctx.hex()[:12].decode("utf-8")
 
     embed = {
         "url": secrets["hgwebUrl"] + "rev/" + shortId,
         "fields": [
-            { "name": "Branch", "value": ctx.branch(), "inline": True },
-            { "name": "Author", "value": ctx.user(), "inline": True },
+            { "name": "Branch", "value": ctx.branch().decode("utf-8"), "inline": True },
+            { "name": "Author", "value": ctx.user().decode("utf-8"), "inline": True },
             { "name": "Node", "value": shortId, "inline": True }
         ]
     }
 
     description = ctx.description().strip()
-    lineBreak = description.find("\n")
+    lineBreak = description.find(b"\n")
     if lineBreak >= 0:
         title = description[:lineBreak].strip()
         description = description[lineBreak:].strip()
@@ -41,9 +41,9 @@ def incoming(ui, repo, node, **kwargs):
         description = None
 
     if title:
-        embed["title"] = title
+        embed["title"] = title.decode("utf-8")
     if description:
-        embed["description"] = description
+        embed["description"] = description.decode("utf-8")
 
     request = urllib.request.Request(secrets["webhookUrl"],
         json.dumps({ "embeds": [embed] }).encode("utf-8"),
@@ -52,5 +52,5 @@ def incoming(ui, repo, node, **kwargs):
     try:
         urllib.request.urlopen(request)
     except urllib.error.URLError as ex:
-        ui.write("Discord incoming hook web request failed because " + str(ex))
+        ui.write(("Discord incoming hook web request failed because " + str(ex) + "\n").encode("utf-8"))
         return
